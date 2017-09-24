438085f2e1ffee7ad35a0fe1d38a760bc68f498a
quentinLeDilavrec <ledilavrecquentin@gmail.com>
2017-09-24 12:12:02 +0200

    automating of hanoi solve

--- hanoi.ml
+++ hanoi.ml
@@ -1,29 +1,34 @@
-
-
-
+type disc = int;;
+type pole = disc list;;
 
 let pole = Array.make 3 [];;
 
-class counter = object
+
+
+let counter = object
   val mutable count=0
-  method step = count <- count+1
-  method get = count
-  end;;
+  method step() = count <- count+1
+  method set_to t = count <- t
+  method get() = count
+end;;
+
+(* Print a move *)
+let print_move origin destination =
+  print_string ("--> move a disc from rod " ^ (string_of_int origin) ^ " to rod " ^ (string_of_int destination));
+  print_newline ();;
 
 let rec make_tower h =
   if h==0 then [] else
     h::(make_tower (h-1));;
 
-let rec read_list f l =
-  match l with
-    [] -> ()
-  | a::b -> 
-    begin
-      f a;
-      read_list f b;
-    end;;
+let print_state() =
+  Array.iter (fun x -> 
+      begin 
+        if x==[] then print_string "|" 
+        else List.iter print_int (List.rev x);
+        print_newline();
+      end) pole;;
 
-Array.set pole 0 (List.rev(make_tower 5));;
 
 exception My_Error;;
 
@@ -31,19 +36,39 @@ let move ori dest =
   match pole.(ori) with
     []-> print_string ("there is no disk to take on the pole " ^ string_of_int ori ^ "\n")
   |o1::o-> if pole.(dest)!=[] && o1> List.hd(pole.(dest)) 
-  then print_string ("you can't put a disk bigger than the one under " ^ string_of_int dest ^ "\n")
+    then print_string ("you can't put a disk bigger than the one under " ^ string_of_int dest ^ "\n")
     else begin
       Array.set pole ori o;
       Array.set pole dest (o1::pole.(dest));
-      print_string ("you've moved a disk from " ^ string_of_int ori ^ " to " ^ string_of_int dest ^ "\n");
-      Array.iter (fun x -> 
-      begin 
-        if x==[] then print_string "|" 
-        else read_list print_int (List.rev x);
-        print_newline();
-      end) pole;
-          end;;
+      print_move ori dest;
+      print_state();
+    end;;
 
-move 0 1;;
-move 0 2;;
-move 1 2;;
+(* Recursively solve the Hanoi towers problem.
+   height is the height of the tower to move from src to dst, and other is the middle rod *)
+let rec hanoi height src other dst =
+    begin
+      counter#step();
+      if height = 1
+      then move src dst
+      else begin
+        hanoi (height - 1) src dst other;
+        move src dst;
+        hanoi (height - 1) other src dst;
+      end
+    end;;
+
+let solve_hanoi pole height src dst =
+  begin
+    Array.set pole src (List.rev(make_tower height));
+    print_state();
+    let rec findi f l i  =
+      if l==[] then -1
+      else if f i then i
+      else findi f (List.tl l) (i+1) in
+    let other = findi (fun x-> x!=src && x!=dst) (Array.to_list pole) 0 in
+    hanoi height src other dst;
+  end;;
+
+solve_hanoi pole 9 0 2;;
+print_string ("Total number of moves: " ^ (string_of_int (counter#get())));;
--- hanoi_extended.ml
+++ hanoi_extended.ml
@@ -8,46 +8,76 @@ type pole = disc list;;
 let width = 1900;;
 let height = 1000;;
 
-open_graph (" "^(string_of_int width)^"x"^(string_of_int height)^"+0-0");;
 
-let utf8_char_to_comprensive_string c =
-  match int_of_char c with
-    n when n<10  -> "\\00" ^ string_of_int n;
-  | n when n<100 -> "\\0"  ^ string_of_int n;
-  | n            -> "\\"   ^ string_of_int n;;
-
-class counter = object
-  val mutable count=0
-  method step = count <- count+1
-  method get = count
-end;;
+class counter = 
+  object
+    val mutable count=0
+    method step = count <- count+1
+    method get = count
+  end;;
 
 let rec make_tower h =
-  if h==0 then [] else
-    (h::(make_tower (h-1)):pole);;
+  if h==0 then [] 
+  else h::(make_tower (h-1):pole);;
 
-let rec read_list f l =
-  match l with
-    [] -> ()
-  | a::b -> 
-    begin
-      f a;
-      read_list f b;
-    end;;
+let move game_state ori dest =
+  if ori >= Array.length game_state || dest >= Array.length game_state 
+  then draw_string "<no pole here>"
+  else
+    begin clear_graph();
+      match game_state.(ori) with
+        []-> draw_rect 0 0 10 10; (*no disk*)
+      |o1::_ when game_state.(dest)!=[] && o1> List.hd(game_state.(dest)) -> 
+        draw_rect 100 100 110 110;
+      |o1::o->
+        begin
+          Array.set game_state ori o;
+          Array.set game_state dest (o1::game_state.(dest));
+          Array.iter (fun x -> 
+              begin 
+                if x==[] then draw_string "|" 
+                else List.iter (fun x-> x |> string_of_int |> draw_string) (List.rev x);(*printing current pole in graphique*)
+              end) game_state;
+        end;
+    end;
 
 exception My_Error;;
 
+class virtual ['a] window height width =
+  let _ =  
+    open_graph (" "^(string_of_int width)^"x"^(string_of_int height)^"+0-0") in
+  object(self)
+
+    val virtual mutable state : 'a
+
+    val height=height
+    val width=width
+
+    method draw_scene () = draw_string "nothing";
+
+    method handler (x:Graphics.status) = ();
+
+    method start (starting_state: 'a) =
+      begin
+
+        (loop_at_exit [Key_pressed] self#handler);
+      end;
+
+
+  end;;
 
 let game = object(self)
 
-  val mutable game_state : pole array = [||];
+  inherit ['a] window height width as super
+
+  val mutable state : pole array = [||];
   val mutable last_tower:int = -1;
 
   method private draw_scene ()=
-  let e = 10 in
-  let dim_max = 150 in
-  let x_base = 10 in
-  let y_base = 10 in
+    let e = 10 in
+    let dim_max = 150 in
+    let x_base = 10 in
+    let y_base = 10 in
     let rec aux p x=
       let rec draw_pole d x y =
         let draw_disc dim x y e =
@@ -62,32 +92,13 @@ let game = object(self)
       match p with
       | [] -> ();
       | pole::pole_list -> 
-      begin 
-        draw_pole pole x y_base;
-        aux pole_list (x+dim_max)
-      end in
-    aux (Array.to_list game_state) x_base
+        begin 
+          draw_pole pole x y_base;
+          aux pole_list (x+dim_max)
+        end in
+    aux (Array.to_list state) x_base
 
-  method move ori dest =
-    if ori >= Array.length game_state || dest >= Array.length game_state 
-    then draw_string "<no pole here>"
-    else
-      begin clear_graph();
-        match game_state.(ori) with
-          []-> draw_rect 0 0 10 10; (*no disk*)
-        |o1::_ when game_state.(dest)!=[] && o1> List.hd(game_state.(dest)) -> 
-          draw_rect 100 100 110 110;
-        |o1::o->
-          begin
-            Array.set game_state ori o;
-            Array.set game_state dest (o1::game_state.(dest));
-            Array.iter (fun x -> 
-                begin 
-                  if x==[] then draw_string "|" 
-                  else read_list (fun x-> x |> string_of_int |> draw_string) (List.rev x);
-                end) game_state;
-          end;
-      end;
+  method move = move state
 
   method private handler x =
     let btw n lower greeter =
@@ -105,7 +116,7 @@ let game = object(self)
           draw_circle 500 500 10;
           self#draw_scene();
         end;
-    | other_key-> draw_string ((utf8_char_to_comprensive_string other_key)^" ");
+    | other_key-> draw_string ((Char.escaped other_key)^" ");
 
   method start game =
     begin
